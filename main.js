2f2013702683735ce1edfa4611988e4856435cdb
Chris <christopherm4rsh@gmail.com>
2023-02-15 17:06:57 +1100

    removed unnecessary html element and unnecessary js variable

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 function rollDice(){
   const diceType = document.getElementById("dice-type").value;
   const diceValue = Math.floor((Math.random()*Number(diceType))+1);
-  const diceImageLink = `images/d${diceType}-${diceValue}-icon.png`;
   const diceImage = document.querySelector("img");
   const diceHeader = document.getElementById("dice__number--header");
   const newDiceHeader = document.getElementById("new__dice");
