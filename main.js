dbe0fb9aa2dc16b1578909140bcbcc29c1ce4d93
Chris <christopherm4rsh@gmail.com>
2023-02-15 16:56:27 +1100

    fixed D in header

--- main.js
+++ main.js
@@ -3,5 +3,9 @@ function rollDice(){
   const diceValue = Math.floor((Math.random()*Number(diceType))+1);
   const diceImageLink = `images/d${diceType}-${diceValue}-icon.png`;
   const diceImage = document.querySelector("img");
+  const diceHeader = document.getElementById("dice__number--header");
+  const newDiceHeader = document.getElementById("new__dice");
   diceImage.src = `images/d${diceType}-${diceValue}-icon.png`;
+  diceHeader.style.display = "none";
+  newDiceHeader.textContent= `D${diceType}`;
 }
